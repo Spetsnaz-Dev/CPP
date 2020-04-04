aff90e1991a8ab73f584e288afd057f9a4cd0339
Spetsnaz <ravindrafk@gmail.com>
2020-04-04 17:29:32 +0530

    LeetCode 30Day Challenge

---
+++ LeetCode/30DaysCodingChallenge/3.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int meh = 0, msf = INT_MIN;
+        
+        for(int i=0; i<nums.size(); i++){
+            meh += nums[i];
+            
+            if(meh < nums[i])
+                meh = nums[i];
+            if(msf < meh)
+                msf = meh;
+        }
+        return msf;
+    }
+};
